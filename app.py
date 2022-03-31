5abb5dd4c209fe873e9b49ce6144ae25b4c09e12
yycheung <yycheung@Galaxy.local>
2022-03-31 14:46:23 -0700

    created Precipitation Route

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ print(tables)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
+session = Session(engine)
+
 ############# Set Up Flask #############
 app = Flask(__name__)
 
@@ -39,3 +41,10 @@ def welcome():
     /api/v1.0/temp/start/end
     ''')
     
+# Precipitation Route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
